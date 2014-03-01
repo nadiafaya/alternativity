59858fe31222dd32279fa9846a1d478a9eb712bf
Nadia Faya <nadiafaya@gmail.com>
2014-03-01 14:36:22 -0200

    Create subject.js

---
+++ subject.js
@@ -0,0 +1,61 @@
+var subjectColors = {
+    availableColors: ['#75CAEB', '#A01CFF', '#FF851B', '#28B62C','#158CBA', '#FF4136'],
+    getColor: function() {
+        if (this.availableColors.length) {
+            return this.availableColors.pop();
+        } else{
+            // random color
+            return '#'+Math.floor(Math.random()*16777215).toString(16);
+        }
+    }
+};
+
+var Day = function (params) {
+    this.name = params.name || ''; // Lu,Ma,Mi,Ju,Vi,Sá
+    this.turn = params.turn || ''; // m, t, n
+};
+
+var Schedule = function () {
+    this.days = [];
+};
+
+var Subject = function (params) {
+    var subject = {}; 
+    var schedulesText = cleanAccents(params.schedules); // Esto es por "Sábado"
+    subject.name = params.name || '';
+    subject.color = subjectColors.getColor();
+    subject.schedules = [];
+
+    function parseSchedulesText () {
+        var parsedSchedules = schedulesText.match(/\w\d\d\d\d.*/gm);
+        for (var i = 0; i < parsedSchedules.length; i++) {
+            var schedule = new Schedule();
+            var parsedDays = parsedSchedules[i].match(/(Lu|Ma|Mi|Ju|Vi|Sa)\((m|t|n)\)/g); // Lu(m)
+            for (var j = 0; j < parsedDays.length; j++) {
+                var dayName = parsedDays[j].match(/Lu|Ma|Mi|Ju|Vi|Sa/g)[0];
+                var dayTurn = parsedDays[j].match(/m|t|n/g)[0];
+                var day = new Day({
+                    name: dayName,
+                    turn: dayTurn
+                });
+                schedule.days.push(day);
+            }
+            subject.schedules.push(schedule);
+        }
+    }
+
+    function cleanAccents (text) {
+        if (text) {
+            return text.replace(/\u00E1/g, 'a')
+                .replace(/\u00E9/g, 'e')
+                .replace(/\u00ED/g, 'i')
+                .replace(/\u00F3/g, 'o')
+                .replace(/\u00FA/g, 'u');
+        } else{
+            return '';
+        }
+    }
+
+    parseSchedulesText();
+    return subject;
+};
